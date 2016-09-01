2a1b891265b84d4dedd6be394251a7049f4d5d91
Rusik <ruslan.yarosh@bk.ru>
2016-09-01 14:54:59 +0300

    do task 2

--- task_sloboda.rb
+++ task_sloboda.rb
@@ -15,4 +15,41 @@ module Sloboda
   end
 
 diagonal([[1,10,12,12],[1,10,12,12],[1,10,12,12],[1,10,12,12]])
+
+  def sort_array(array)
+  index_max=0
+  index_min=0
+  i=0
+  k=0
+  n=array.length
+  max=array.max
+  min=array.min
+    while !(max == array[i])
+      index_max=index_max + 1
+      i+=1
+    end
+  
+ # puts index_max
+  
+    while !(min == array[k])
+      index_min=index_min + 1
+      k+=1
+    end
+
+# puts index_min
+ 
+   index_sort=[index_max, index_min].sort
+   get_part_of_array=array[(index_sort[0]+1) .. (index_sort[1]-1)].sort
+ 
+ # puts get_part_of_array
+ 
+   first_part=array[0 .. (index_sort[0])]
+   last_part=array[index_sort[1] .. (n-1)]
+ 
+   result = first_part + get_part_of_array + last_part
+ 
+   puts result
+end 
+ 
+ sort_array([12,3,45,88,33,55,65,77,75,31,1,79]
 end
